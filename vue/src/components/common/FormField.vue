f9908a0ab251a2ec56be51274423983e662140b7
Abdun Nafi <nafichasani@gmail.com>
2024-12-23 00:10:25 +0700

    fix: form field select and add other type

--- vue/src/components/common/FormField.vue
+++ vue/src/components/common/FormField.vue
@@ -2,15 +2,23 @@
     <div class="mb-4">
         <label :for="id" class="block text-sm font-medium text-secondary mb-2">{{ label }}</label>
 
-        <!-- Text Input -->
-        <input v-if="type === 'text'" :id="id" :value="modelValue" :placeholder="placeholder" :required="required"
-            @input="$emit('update:modelValue', $event.target.value)"
+        <!-- General Input Types -->
+        <input v-if="['text', 'number', 'email', 'password', 'date'].includes(type)" 
+            :id="id" 
+            :type="type" 
+            :value="modelValue" 
+            :placeholder="placeholder" 
+            :required="required"
+            @input="$emit('update:modelValue', type === 'number' ? $event.target.valueAsNumber : $event.target.value)"
             class="block w-full text-sm border border-gray-300 rounded-md shadow-sm focus:ring-primary focus:border-primary px-3 py-2 placeholder-secondary focus:outline-none" />
 
         <!-- Select Dropdown -->
-        <select v-if="type === 'select'" :id="id" :value="modelValue" @change="$emit('update:modelValue', $event.target.value)"
-                :required="required"
-                class="block w-full text-sm border border-gray-300 rounded-md shadow-sm focus:ring-primary focus:border-primary px-3 py-2 placeholder-secondary focus:outline-none">
+        <select v-if="type === 'select'" 
+            :id="id" 
+            :value="modelValue" 
+            @change="$emit('update:modelValue', $event.target.value)"
+            :required="required"
+            class="block w-full text-sm border border-gray-300 rounded-md shadow-sm focus:ring-primary focus:border-primary px-3 py-2 placeholder-secondary focus:outline-none">
             <!-- Placeholder option -->
             <option v-if="placeholder" value="" disabled selected>{{ placeholder }}</option>
             <!-- Options -->
@@ -25,11 +33,15 @@
 defineProps({
     id: { type: String, required: true },
     label: { type: String, required: true },
-    type: { type: String, default: 'text' }, // 'text', 'select', etc.
+    type: { 
+        type: String, 
+        default: 'text', // Default type is 'text'
+        validator: value => ['text', 'number', 'email', 'password', 'date', 'select'].includes(value) // Supported types
+    },
     options: { type: Array, default: () => [] }, // For 'select' type, array of objects { label, value }
     placeholder: { type: String, default: '' },
     required: { type: Boolean, default: false },
-    modelValue: [String, Number, Object], // :model value
+    modelValue: [String, Number, Object], // Supports binding to various data types
 });
 
 defineEmits(['update:modelValue']);
