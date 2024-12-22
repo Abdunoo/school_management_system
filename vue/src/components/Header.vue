0cdb532745f81b8db6f0169f79d683571741c0b6
Abdun Nafi <nafichasani@gmail.com>
2024-12-23 00:10:50 +0700

    fix: header styling responsive

--- vue/src/components/Header.vue
+++ vue/src/components/Header.vue
@@ -11,20 +11,20 @@ defineProps<{
 </script>
 
 <template>
-  <header class="bg-white border-b px-4 sm:px-6 py-4 rounded-b-3xl">
-    <div class="flex items-center justify-between flex-wrap">
+  <header class="bg-white border-b px-4 sm:px-6 py-2 rounded-b-3xl">
+    <div class="flex items-center justify-between ">
       <div class="flex items-center space-x-4">
         <Bars3BottomLeftIcon class="w-6 h-6 text-gray-600 cursor-pointer" @click="toggleSidebar" />
         <p class="text-lg md:text-xl font-semibold text-gray-800">School Management System</p>
       </div>
 
-      <div class="hidden md:flex items-center space-x-4 mt-2 ">
-        <button class="p-2 hover:bg-gray-100 rounded-full">
+      <div class="flex items-center space-x-4 ">
+        <button class="hidden md:flex p-2 hover:bg-gray-100 rounded-full">
           <BellIcon class="w-6 h-6 text-gray-600" />
         </button>
         <button class="flex items-center space-x-2 hover:bg-gray-100 rounded-lg px-3 py-2">
           <UserCircleIcon class="w-6 h-6 text-gray-600" />
-          <span class="text-sm text-gray-700">Admin</span>
+          <span class="hidden md:flex text-sm text-gray-700">Admin</span>
         </button>
       </div>
     </div>
